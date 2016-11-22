c2b974fe4def76a714c419ec7fe40f4beb15ef94
Oleg Kozin <olegkosin2@yandex.ru>
2016-11-22 14:50:59 +0300

    Дичь

--- Lab5/WindowsFormsApplication1/Dat.cs
+++ Lab5/WindowsFormsApplication1/Dat.cs
@@ -8,5 +8,33 @@ namespace WindowsFormsApplication1
 {
     class Dat
     {
+       // private List<List<double>> Data_point_list = new List<List<double>>();
+       // private List<double> row = new List<double>();
+
+        private struct Data_point
+        {
+            private double x=0;
+            private double y=0;
+
+            private double X { get { return x; } set { x = value; } }
+            private double Y { get { return y; } set { y = value; } }
+
+            public Data_point(double x, double y)
+            {
+
+            }
+        }
+
+        private List<Data_point> Data_point_list = new List<Data_point>();
+
+        public void Add_data_point(double x, double y)
+        {
+            Data_point cur = new Data_point(x,y);
+
+            
+            Data_point_list.Add(x,y)
+        }
+
+     
     }
 }
